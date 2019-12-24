09e5d435450895e49e96cc58453db039b6ad2631
VolodymyrKalakura <kalakura.volodymyr@gmail.com>
2019-12-24 14:16:14 +0200

    [fix] small refactoring

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,21 +2,18 @@
 let a = +prompt('Enter value A', '');
 let b = +prompt('Enter value B', '');
 let c = +prompt('Enter value C', '');
-
-if (isNaN(a) || isNaN(b) || isNaN(c)) {
-	console.log ('Invalid input data');
-} 
-
 let discr = Math.pow(b, 2) - 4 * a * c;
 let sqrtDiscr = Math.sqrt(discr);
 let x1 = parseInt((-b - sqrtDiscr) / (2 * a));
 let x2 = parseInt((-b + sqrtDiscr) / (2 * a));
-if (a === 0) {
+
+let output = (a === 0) ? 'Invalid input data' :
+				(discr < 0) ? 'no solution' :
+				(discr === 0) ? 'x = ' + x1 :
+				`x1 = ${x1}, x2 = ${x2}`;
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
 	console.log ('Invalid input data');
-} else if (discr < 0) {
-	console.log ('no solution');
-} else if (discr === 0) {
-	console.log('x = ' + x1);
-} else if (discr > 0){
-	console.log (`x1 = ${x1}, x2 = ${x2}`);
+} else {
+	console.log(output);
 }
